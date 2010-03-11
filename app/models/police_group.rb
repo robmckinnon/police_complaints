94b53f200c1afab37dce3c5cf5e23315f9edf28c
robmckinnon <rob@theyworkforyou.co.nz>
2010-03-11 14:48:50 +0000

    formatting

--- app/models/police_group.rb
+++ app/models/police_group.rb
@@ -1,5 +1,17 @@
 class PoliceGroup < ActiveRecord::Base
   
   has_friendly_id :name, :use_slug => true, :strip_diacritics => true
-  
+
+  def rank
+    groups = PoliceGroup.all.sort_by(&:grand_total)
+    index = groups.index(self) + 1
+    place = index / groups.size.to_f
+    if place > 0.75
+      'High'
+    elsif place > 0.25
+      'Moderate'
+    else
+      'Low'
+    end
+  end
 end
